bf7dec734b622f034d5727b66704d1d383b72002
huntleyfrancesta <huntleyfrancesta@gmail.com>
2021-07-24 17:52:29 -0400

    done

--- lib/seed.sql
+++ lib/seed.sql
@@ -1,9 +1,32 @@
-INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
-(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
-(2,"Rowdy", 10, "M", "black", "intense", 1),
-(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
-(4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
-(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
-(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
-(7,"Wendy", 6, "F", "Blue", "naive", 1),
-(8,null, 20, "M", "black", "aggressive", 0);
+def selects_all_female_bears_return_name_and_age
+  "select bears.name, bears.age from bears where sex = 'F';"
+
+end
+
+def selects_all_bears_names_and_orders_in_alphabetical_order
+  "select bears.name from bears order by name;"
+end
+
+def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
+  "select bears.name, bears.age from bears where alive = 1 order by age asc;"
+end
+
+def selects_oldest_bear_and_returns_name_and_age
+  "select bears.name, bears.age from bears order by age desc limit 1;"
+end
+
+def select_youngest_bear_and_returns_name_and_age
+  "select bears.name, bears.age from bears order by age limit 1;"
+end
+
+def selects_most_prominent_color_and_returns_with_count
+  "select bears.color, count(bears.color) as color_prominence from bears group by bears.color order by color_prominence desc limit 1;"
+end
+
+def counts_number_of_bears_with_goofy_temperaments
+  "select count(bears.color) from bears where temperament = 'goofy';"
+end
+
+def selects_bear_that_killed_Tim
+  "select * from bears where name is null;"
+end
